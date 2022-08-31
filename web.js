bba44b6bde6784aa61cf611e2b517fec89409c14
huouvcti <huouvcti@gmail.com>
2022-08-31 18:54:56 +0900

    hello cafe24

---
+++ web.js
@@ -0,0 +1,20 @@
+"use strict";
+
+require('dotenv').config({ path: 'a.env'});
+
+const app = require('./app');
+const http = require('http');
+
+// const {socketio} = require('./middleware/socketio');
+
+const port = process.env.PORT || process.env.S_PORT || 3000;
+const server = http.createServer(app);
+server.listen(port, () => {
+    console.log('Server on ' + port);
+});
+
+// socketio(server, app);
+
+module.exports = server;
+
+
